21832ca1ff066a6324b7f4b3f153874b43b45620
20kdc <asdd2808@gmail.com>
2017-10-11 16:34:41 +0100

    Reformat, optimize imports, move to CC0

--- src/main/java/gabien/GrInDriver.java
+++ src/main/java/gabien/GrInDriver.java
@@ -1,11 +1,12 @@
 /*
- * This is released into the public domain.
- * No warranty is provided, implied or otherwise.
+ * gabien-javase - gabien backend for desktop Java
+ * Written starting in 2016 by contributors (see CREDITS.txt)
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+ * You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
 
 package gabien;
 
-import gabien.backendhelp.INativeImageHolder;
 import gabien.backendhelp.ProxyGrDriver;
 
 import javax.swing.*;
@@ -14,7 +15,6 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.util.HashSet;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
